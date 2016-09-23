e9e76dde8f31aeb50f0b1e09e5a59d32444d1297
foukLor <vitaminotar@yandex.com>
2016-09-23 16:55:32 +0300

    add new functions to find bars

--- bars.py
+++ bars.py
@@ -1,21 +1,41 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath,'r') as data_file:
+        data = json.load(data_file)
+    return data
+
 
 
 def get_biggest_bar(data):
-    pass
+    max_bar = max(data, key = lambda event: event['Cells']['SeatsCount'])
+    return max_bar['Cells']['Name']
 
 
 def get_smallest_bar(data):
-    pass
+    min_bar = min(data, key = lambda event: event['Cells']['SeatsCount'])
+    return min_bar['Cells']['Name']
 
 
+#we use eucledian metrics
 def get_closest_bar(data, longitude, latitude):
-    pass
+    minD = min(data,key= lambda bar: ((longitude - bar['Cells']['geoData']['coordinates'][0])**2
+         + (latitude - bar['Cells']['geoData']['coordinates'][1])**2 )**(1.0/2))
+    return minD['Cells']['Name']
+
 
 
 if __name__ == '__main__':
-    pass
+    if  len(sys.argv) != 2:
+        print(" Usage:python3.5 bars.py file path to json")
+    x_gps = float(input("longitude:"))
+    y_gps = float(input("latitude:"))
+    data = load_data(sys.argv[1])
+    max_bar = get_biggest_bar(data)
+    min_bar = get_smallest_bar(data)
+    d = get_closest_bar(data, x_gps, y_gps)
+    print(max_bar)
+    print(d)
+    print(min_bar)
